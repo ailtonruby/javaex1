bc814f70058e5518bf115de6c7c4e224ad7b2d4d
ailtonruby <ailtonruby97@gmail.com>
2024-08-15 21:06:13 -0300

    exercicio

---
+++ src/Carro.java
@@ -0,0 +1,16 @@
+public class Carro {
+    // Atributos (características)
+    public double velocidade;
+
+    public double getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(double velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public void velocidadeCarro(double velocidade){
+        setVelocidade(velocidade);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        Carro meuCarro = new Carro();
+        meuCarro.setVelocidade(33);
+        System.out.println(meuCarro.getVelocidade());
+
+    }
+}
